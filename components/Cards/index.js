a0ae5c7d01d618196fc762830b73b7b11b44230d
Julie Thrall <juliethrall10@gmail.com>
2019-07-12 10:54:42 -0700

    completed cards component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,111 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container')
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res => {
+        //console.log(res.data.articles.bootstrap[0].headline)
+        //console.log(res.data.articles)
+        const articles = res.data.articles
+
+        const javaScript = 'javascript'
+        const bootstrap = 'bootstrap'
+        const technology = 'technology'
+        const jquery = 'jquery'
+        const node = 'node'
+
+
+        for (let i in articles) {
+            if (i.includes(`${javaScript}`)) {
+                let items = []
+                items = res.data.articles.javascript
+                for (let item in items) {
+                    console.log(items[item])
+                    const element = createCard(items[item])
+                    console.log(cardsContainer)
+                    cardsContainer.appendChild(element)
+
+                }
+            }
+            if (i.includes(`${bootstrap}`)) {
+                let items = []
+                items = res.data.articles.bootstrap
+                for (let item in items) {
+                    console.log(items[item])
+                    const element = createCard(items[item])
+                    console.log(cardsContainer)
+                    cardsContainer.appendChild(element)
+                }
+            }
+            if (i.includes(`${technology}`)) {
+                let items = []
+                items = res.data.articles.technology
+                for (let item in items) {
+                    console.log(items[item])
+                    const element = createCard(items[item])
+                    console.log(cardsContainer)
+                    cardsContainer.appendChild(element)
+                }
+            }
+            if (i.includes(`${jquery}`)) {
+                let items = []
+                items = res.data.articles.jquery
+                for (let item in items) {
+                    console.log(items[item])
+                    const element = createCard(items[item])
+                    console.log(cardsContainer)
+                    cardsContainer.appendChild(element)
+                }
+            }
+            if (i.includes(`${node}`)) {
+                let items = []
+                items = res.data.articles.node
+                for (let item in items) {
+                    console.log(items[item])
+                    const element = createCard(items[item])
+                    console.log(cardsContainer)
+                    cardsContainer.appendChild(element)
+                }
+            }
+        }
+    })
+    .catch(e => {
+        console.log(e)
+    })
+
+
+function createCard(item) {
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const headline = document.createElement('div')
+    headline.classList.add('headline')
+    headline.textContent = item.headline
+
+    const author = document.createElement('div')
+    author.classList.add('author')
+    author.textContent = item.authorName
+
+    const imgContainer = document.createElement('div')
+    imgContainer.classList.add('img-container')
+
+    const image = document.createElement('img')
+    image.src = item.authorPhoto
+
+    const authorName = document.createElement('span')
+
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(authorName)
+    author.appendChild(imgContainer)
+    imgContainer.appendChild(image)
+
+
+    return card
+
+
+}
